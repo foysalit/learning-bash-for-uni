cfcab0c51d082666e8ef74f6147b51e840f781d9
Foysal Ahamed <cfaion341@gmail.com>
2015-01-07 19:18:17 +0100

    function with multi param and checking null value of param using -n

--- functions/playground.sh
+++ functions/playground.sh
@@ -16,4 +16,13 @@ param_greetings() {
 }
 param_greetings Foysal
 
+null_checker() {
+	if [ -n "$1" ]; then
+		echo "first param $1"
+	fi
 
+	if [ -n "$2" ]; then
+		echo "Second param $2"
+	fi
+}
+null_checker Bla Bla
