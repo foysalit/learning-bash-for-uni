e70dc9a89ea0f0f83cc936b252431969d6e74067
Foysal Ahamed <cfaion341@gmail.com>
2015-02-06 01:31:05 +0100

    background jobs creator and destroyer complete

--- progs/background_jobs.sh
+++ progs/background_jobs.sh
@@ -5,8 +5,11 @@ PIDS=""
 
 while [ $COUNTER -le $1 ]; do
 	echo counter is: $COUNTER
-	ls  >"~/tmp/out$COUNTER.txt" &
+	#ls > "/home/foysal/tmp/out$COUNTER.txt" &
+	watch -n 1 "ps aux | grep test" > "/home/foysal/tmp/out3.txt" &
 	PIDS="$PIDS $!"
-	echo "NEW PID: $PIDS"
 	COUNTER=$((COUNTER+1))
 done
+
+echo "killing processes: $PIDS"
+kill -9 $PIDS
