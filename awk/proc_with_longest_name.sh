7886809bf7ae24367ea1bb04621d27f3dbef6456
Foysal Ahamed <cfaion341@gmail.com>
2015-02-12 16:34:33 +0100

    process_with_longest_name_by_user is working

--- awk/proc_with_longest_name.sh
+++ awk/proc_with_longest_name.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+TMP_FILE="/tmp/proc_with_longest_name.tmp"
+
 ps aux | 
 awk '
 BEGIN{
 	FS=" "
 }
 {
-	#split($11,PROCNAME," ")
-	if ($1=="'$1'") {print $11}
+	split($11,PROCNAME," ")
+	if ($1=="'$1'") {print PROCNAME[1],length(PROCNAME[1])}
 }' | 
-sort -nk 1
+sort -rnk 2 > $TMP_FILE
+
+head -n 1 $TMP_FILE | awk '{print $1}'
+
+rm $TMP_FILE
