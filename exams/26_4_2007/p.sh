60d5c59dc4261b586722a14636bc039adb5cc6cb
Foysal Ahamed <cfaion341@gmail.com>
2015-02-20 13:23:26 +0100

    added another exam solution

--- exams/26_4_2007/p.sh
+++ exams/26_4_2007/p.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+PIDS=$!
+FILENAME_BASE="out"
+
+if [ -z $1 ]; then
+	echo "Must pass a number argument"
+	exit 1
+fi
+
+for (( i = 1; i <= $1; i++ )); do
+	ls > "$FILENAME_BASE$i" &
+	PIDS="$PIDS $!"
+done
+
+for id in $PIDS; do
+	sleep 3
+	echo "Killing job with id: $id"
+	kill "$id"
+done
